f80b43c31db7a179f0863955ac9f7d202884c60a
Mihail Ferym <drakomishel@gmail.com>
2025-03-08 21:56:41 +0300

    game_test

---
+++ public/game.test.js
@@ -0,0 +1,29 @@
+const { PacMan } = require('./game') // Пусть будет модуль, который мы создадим для логики
+
+describe('PacMan', () => {
+	let pacMan
+
+	beforeEach(() => {
+		pacMan = new PacMan()
+	})
+
+	test('should move left when ArrowLeft is pressed', () => {
+		pacMan.move('ArrowLeft')
+		expect(pacMan.x).toBeLessThan(250)
+	})
+
+	test('should move right when ArrowRight is pressed', () => {
+		pacMan.move('ArrowRight')
+		expect(pacMan.x).toBeGreaterThan(250)
+	})
+
+	test('should move up when ArrowUp is pressed', () => {
+		pacMan.move('ArrowUp')
+		expect(pacMan.y).toBeLessThan(250)
+	})
+
+	test('should move down when ArrowDown is pressed', () => {
+		pacMan.move('ArrowDown')
+		expect(pacMan.y).toBeGreaterThan(250)
+	})
+})
